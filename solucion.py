7458e4801dfe2bdfb359a77ca3ada5f529f11a3c
Juan Pablo Pasos <itzsirpoul3@gmail.com>
2025-11-28 18:29:55 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -1,20 +1,20 @@
 # En este archivo debes implementar la función
 
 def reloj_arena(m: int, s: str) -> str:
-
-
+    # Validar altura mayor que 0
     if m <= 0:
         print("Error: La altura debe ser un entero positivo")
         return
-
-    caracter = s[0]
     
     for i in range(m):
         espacios = i
         num_chars = (2 * m - 1) - (2 * i)
-        print(' ' * espacios + caracter * num_chars)
-        
+
+        print(' ' * espacios + s * num_chars)
+    
+
     for i in range(m - 2, -1, -1):
         espacios = i
         num_chars = (2 * m - 1) - (2 * i)
-        print(' ' * espacios + caracter * num_chars)
+
+        print(' ' * espacios + s * num_chars)
