bf24004740bbb9cc48bb6094957271dda42c4c13
Juan Pablo Pasos <itzsirpoul3@gmail.com>
2025-11-29 11:21:02 -0700

    Implement width and block calculations in reloj_arena
    Added calculations for total width and number of blocks for the hourglass pattern.

--- solucion.py
+++ solucion.py
@@ -1,20 +1,25 @@
-# En este archivo debes implementar la función
-
 def reloj_arena(m: int, s: str) -> str:
-    # Validar altura mayor que 0
     if m <= 0:
         print("Error: La altura debe ser un entero positivo")
         return
     
-    for i in range(m):
-        espacios = i
-        num_chars = (2 * m - 1) - (2 * i)
-
-        print(' ' * espacios + s * num_chars)
+    len_s = len(s)
     
-
+ 
+    bloques_iniciales = 2 * m - 1
+    
+    
+    ancho_total_chars = bloques_iniciales * len_s
+    
+    for i in range(m):
+        ancho_actual_chars = ancho_total_chars - (2 * i)
+        
+        num_bloques = ancho_actual_chars // len_s
+        
+        print(' ' * i + s * num_bloques)
+    
     for i in range(m - 2, -1, -1):
-        espacios = i
-        num_chars = (2 * m - 1) - (2 * i)
-
-        print(' ' * espacios + s * num_chars)
+        ancho_actual_chars = ancho_total_chars - (2 * i)
+        num_bloques = ancho_actual_chars // len_s
+        
+        print(' ' * i + s * num_bloques)
